c58030eea9babcae722c25d5895c03a85da0dcc3
charlesdzadu <dkccharlie@gmail.com>
2022-07-06 13:22:23 +0000

    link in home

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Bienvenue. C'est vide oui.. nous sommes entrain de travailler !</div>
+  <div>Bienvenue. C'est vide oui.. nous sommes entrain de travailler !
+
+
+    <div>
+      <nuxt-link to="/cgu/conditions-utilisations">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Les conditions d'utilisations
+        </button>
+      </nuxt-link>
+      <nuxt-link to="politique-de-confidentialite">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          La politique de confidentialité
+        </button>
+      </nuxt-link>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
